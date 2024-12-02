0c24d56f817fa9291f8cfb3890b1959090b03267
Kirsten Andrew <github_cloudlabsuser_1318@cloudlabsaiuser.com>
2024-12-02 15:27:00 +0000

    add app insights config

--- src/ContosoTraders.Api.Products/Program.cs
+++ src/ContosoTraders.Api.Products/Program.cs
@@ -1,1 +1,13 @@
 DependencyInjection.ConfigureApp();
+AddApplicationInsightsTelemetry();
+
+// This method gets called by the runtime. Use this method to add services to the container.
+var builder = WebApplication.CreateBuilder(args);
+
+// The following line enables Application Insights telemetry collection.
+builder.Services.AddApplicationInsightsTelemetry();
+
+// This code adds other services for your application.
+builder.Services.AddMvc();
+
+var app = builder.Build();
